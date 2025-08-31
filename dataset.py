11b30c32dc5ed473845889f82e003389a9b4137e
alvarodca <19acanizo@gmail.com>
2025-08-31 21:28:33 +0200

    New dataset

--- dataset.py
+++ dataset.py
@@ -32,67 +32,17 @@ def thermal_velocity(T,eff_m):
 # Obtaining different elements and formulas to clarify code
 def Nc(T):
     """Effective density of states in the conduction band
-    T: temperature (kelvin)"""
-    return 2*((3*np.pi*mass_n*K_B_J*T)/h**2)**(3/2)
+    T: temperature (kelvin)
+    Divided by 10^6 as we want cm^-3"""
+    return (2*((3*np.pi*mass_n*K_B_J*T)/h**2)**(3/2))/1e6
 
 
 def Nv(T):
     """Effective density of states in the valence band
-    T: temperature (kelvin)"""
-    return 2*((3*np.pi*mass_p*K_B_J*T)/h**2)**(3/2)
+    T: temperature (kelvin)
+    Divided by 10^6 as we want cm^-3"""
+    return (2*((3*np.pi*mass_p*K_B_J*T)/h**2)**(3/2))/1e6
 
-
-# Generating dataset
-# def generate_dataset(N):
-#     """Generates a dataset of N rows"""
-#     e_t = np.random.uniform(-0.55,0.55, N) # Defect Energy Level
-#     sigma_p = 10**np.random.uniform(-17,-13,N) # Capture cross sections p
-#     sigma_n = 10**np.random.uniform(-17,-13,N) # Capture cross section n
-#     k = sigma_n/sigma_p # Capture cross section ratio
-#     delta_n = 10**np.random.uniform(1e13, 1e17, N) # Excess carrier concentration
-#     temperature = np.random.uniform(200,400, N)
-#     alpha = np.random.uniform(1.1,4, N) # Power law coefficient
-#     e_v = np.random.uniform(0.01,0.25) # Activation Energy
-#     p_0 = N_DOP
-
-#     # Thermal velocities
-#     v_n = thermal_velocity(temperature,mass_n) # Electrons
-#     v_p = thermal_velocity(temperature, mass_p) # Holes
-
-#     # Intrinsic carrier concentration
-#     nc = Nc(temperature)
-#     nv = Nv(temperature)
-#     n_i = np.sqrt(nc*nv)*np.exp(-E_G_SI/(2*K_B_J*temperature))
-
-#     n_0 = n_i**2/N_DOP
-#     p_0 = N_DOP
-
-#     # n1 and p1
-#     n_1 = n_i * np.exp(e_t/(K_B_EV*temperature))
-#     p_1 = n_i * np.exp(-e_t/(K_B_EV*temperature))
-
-#     # Trap time constants
-#     tau_p0 = 1/sigma_p*v_p*N_T
-#     tau_n0 = 1/sigma_n*v_n*N_T
-
-#     # SRH
-#     numerator = tau_p0*(n_0 + n_1 + delta_n) + tau_n0*(p_0 + p_1+ delta_n)
-#     denominator = n_0 + p_0 + delta_n
-#     srh = numerator/denominator
-
-#     # Generating the dataset
-#     df = pd.DataFrame({"E_t": e_t, "CS_n": sigma_n, "CS_p": sigma_p, "CS_ratio": k, "Δn": delta_n, "T": temperature,"α": alpha, "E_v": e_v, "SRH": srh })
-
-#     return df
- 
-
-# def show_curves():
-#     """Function to represent the curves from the dataset"""
-#     df = generate_dataset(20)
-
-#     df.plot(x="Δn", y = "SRH", logy=True)
-
-#     plt.show()
 
 def compute_srh(temperature, delta_n, e_t, sigma_n, sigma_p, N_T=1e12, N_DOP = 5.1e15):
     """Computes SRH formula
@@ -112,15 +62,15 @@ def compute_srh(temperature, delta_n, e_t, sigma_n, sigma_p, N_T=1e12, N_DOP = 5
     mass_n = 1.08* M_E # Effective mass of electrons
     mass_p = 0.81 * M_E # Effective mass of holes
 
-    # Thermal velocities
-    v_n = thermal_velocity(temperature,mass_n) # Electrons
-    v_p = thermal_velocity(temperature, mass_p) # Holes
+    # Thermal velocities, in cm/s
+    v_n = thermal_velocity(temperature,mass_n) * 100 # Electrons
+    v_p = thermal_velocity(temperature, mass_p) * 100 # Holes
 
     # Intrinsic carrier concentration
     nc = Nc(temperature)
     nv = Nv(temperature)
-    n_i = np.sqrt(nc*nv)*np.exp(-E_G_SI/(2*K_B_J*temperature))
-
+    n_i = np.sqrt(nc*nv)*np.exp(-E_G_SI/(2*K_B_EV*temperature))
+    
     n_0 = n_i**2/N_DOP
     p_0 = N_DOP
     
@@ -129,8 +79,8 @@ def compute_srh(temperature, delta_n, e_t, sigma_n, sigma_p, N_T=1e12, N_DOP = 5
     p_1 = n_i * np.exp(-e_t/(K_B_EV*temperature))
 
     # Trap time constants
-    tau_p0 = 1/sigma_p*v_p*N_T
-    tau_n0 = 1/sigma_n*v_n*N_T
+    tau_p0 = 1/(sigma_p*v_p*N_T)
+    tau_n0 = 1/(sigma_n*v_n*N_T)
 
     # SRH
     numerator = tau_p0*(n_0 + n_1 + delta_n) + tau_n0*(p_0 + p_1+ delta_n)
@@ -139,51 +89,117 @@ def compute_srh(temperature, delta_n, e_t, sigma_n, sigma_p, N_T=1e12, N_DOP = 5
 
     return srh
 
-def store_curves(temperature, n_points, curves):
+def store_curves(temperatures, n_points, defect_type):
     """Functions which creates curves
+    temperatures <- list of temperatures
     n_points <- Points to represent each curve
-    curves <- amount of curves to create"""
+    defect_type <- Defects {one,two,two_levels}"""
 
     # Initializing excess carrier concentration
     delta_ns = np.logspace(13,17,n_points)
 
-    # Defect level
-    e_t = []
-    # sigmas
-    sigma_p = []
-    sigma_n = []
+    # Assigning defects depending on the defect type
+    if defect_type == "one":
+        n_defects = 1
 
-    for i in range(curves):
-        e_t.append(np.random.uniform(-0.55,0.55))
-        sigma_n.append(10**np.random.uniform(-17,-13))
-        sigma_p.append(10**np.random.uniform(-17,-13))
+    elif defect_type == "two":
+        n_defects = 2
+
+    elif defect_type == "two_levels":
+        n_defects = 2 # One element two levels
+
+    else:
+        raise ValueError("Invalid type")
+    
+    
+    # Defect level combinations 
+    e_t = np.random.uniform(-0.55,0.55,n_defects)
+    sigma_n = 10**np.random.uniform(-17,-13)
+    sigma_p = 10**np.random.uniform(-17,-13)
 
     # Initializing empty dataset
-    df = []
+    rows = []
 
     # Computing parameters
-    for i in range(curves):
-        de = e_t[i]
-        sigman = sigma_n[i]
-        sigmap = sigma_p[i]
+    for t in temperatures:         
+            # Computing srh and storing the result
+            for delta in delta_ns:
+                srh_sum = 0
 
-        for delta in delta_ns:
-            srh = compute_srh(temperature, delta_ns, de, sigman, sigmap, N_T=1e12, N_DOP = 5.1e15)
+                for i in range(n_defects):
+                    srh_sum += compute_srh(t, delta, e_t[i], sigma_n[i], sigma_p[], N_T=1e12, N_DOP = 5.1e15)
+                
+                # Labelling our data
+                if defect_type == "one":
+                    label = 0
+                
+                elif defect_type == "two":
+                    label = 1
 
-            df.append({"E_t": e_t, "CS_n": sigma_n, "CS_p": sigma_p,"Δn": delta_ns, "T": temperature,"SRH": srh })
+                elif defect_type == "two_levels":
+                    label = 2
+
+
+                rows.append({"Δn": delta, "T": t,"SRH": srh_sum, "Label": label})
     
-    return pd.DataFrame(df)
+    df = pd.DataFrame(rows)
+
+    return df
+
+def show_curve():
+    
+    df = pd.read_csv("srh_curves.csv")
+
+    fig, ax = plt.subplots( figsize = (9,6))
+    
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    
+    # Obtaining the set of different temperatures
+    temperatures = sorted(df["T"].unique())
+
+    # Plotting the curve
+    for T in temperatures:
+        subset = df[df["T"] == T]
+
+        ax.plot(subset["Δn"], subset["SRH"], label=f'T={T}K')
+    
+    
+    
+    ax.set_xlabel('Excess carrier concentration')
+    ax.set_ylabel('Lifetime (SRH)')
+    ax.set_title('SRH Lifetime Curve', size = 14)
+    plt.show()
 
 
 if __name__ == "__main__":
-
-    df = store_curves(temperature=300, n_points=200, curves=2)
-
-    df.to_csv("srh_curves.csv", index=False)
-    # dataset = generate_dataset(20)
-    #show_curves()
-    # print("Stored dataset")
-    # dataset.to_csv("dataset.csv", header = False, mode = "a", index = False)
-    #dataset.to_csv("dataset.csv", index = False)
-    # If we wish to append a new line, include variable mode
     
+    # Initializing temperatures
+    temperatures = [200,225,250,275,300,325,350,375,400]
+
+    # Each defect combination is simulated with 9 different temperatures2
+    curves = []
+
+    # Samples per class
+    samples = 1
+    for _ in range(samples):
+        curves.append(store_curves(temperatures,n_points=100,defect_type="one"))
+        curves.append(store_curves(temperatures,n_points=100,defect_type="two"))
+        curves.append(store_curves(temperatures,n_points=100,defect_type="two_levels"))
+
+    df = pd.concat(curves, ignore_index= False)
+    df.to_csv("srh_curves.csv", index=False)
+    # df.to_csv("srh_curves.csv", index=False, mode = "a")
+
+    # Plotting the curve
+    #show_curve()
+
+    # cuando veo una curva quiero saber si una impureza con dos niveles o dos impurezas <- supervised
+    # si es concava una impureza, convexa unknown <- clustering
+    
+
+# Preguntas
+
+# En la grafica, eso es la misma curva o no?
+
+# Supervisado o no supervisado
